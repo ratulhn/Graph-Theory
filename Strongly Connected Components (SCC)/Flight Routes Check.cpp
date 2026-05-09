113d50fd91fce2fd1ed74935288027430a24f4d7
ratulhn <ratulhossain.bd2003@gmail.com>
2026-05-09 12:48:30 +0600

    Create Flight Routes Check.cpp

---
+++ Strongly Connected Components (SCC)/Flight Routes Check.cpp
@@ -0,0 +1,92 @@
+#include <bits/stdc++.h>
+using namespace std;
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+using namespace __gnu_pbds;
+template<class T> 
+using oset = tree<T, null_type, less<T>, rb_tree_tag, tree_order_statistics_node_update>; // find_by_order, order_of_key
+
+#define FastIO ios::sync_with_stdio(false);cin.tie(nullptr);
+#define dbg(v) cout << "Line(" << __LINE__ << ") -> " << #v << " = " << (v) << "\n";
+//#include "dbgtest.h"
+#define ll long long
+#define all(v) v.begin(),v.end()
+#define nl cout<<'\n'
+#define lp(i,a,n) for(int i=a; i<n; i++)
+
+const int M = 1e9+7;
+
+
+
+const int N = 5e5 + 9;
+
+bool vis[N];
+vector<int> g[N], r[N], G[N], vec; //G is the condensed graph
+void dfs1(int u) {
+  vis[u] = 1;
+  for(auto v: g[u]) if(!vis[v]) dfs1(v);
+  vec.push_back(u);
+}
+
+vector<int> comp;
+void dfs2(int u) {
+  comp.push_back(u);
+  vis[u] = 1;
+  for(auto v: r[u]) if(!vis[v]) dfs2(v);
+}
+
+
+int idx[N];
+
+void solve() {
+
+    int n,m; cin>>n>>m;
+    for(int i=0; i<m; i++) {
+        int u,v; cin>>u>>v;
+        g[u].push_back(v);
+        r[v].push_back(u);
+    }
+
+    for(int i=1; i<=n; i++) {
+        if(!vis[i]) dfs1(i);
+    }
+
+    reverse(vec.begin(),vec.end());
+    memset(vis, 0, sizeof vis);
+    int scc = 0;
+    vector<int>res;
+    for(auto u:vec) {
+        if(!vis[u]) {
+            comp.clear();
+            dfs2(u);
+            scc++;
+            for(auto x: comp) idx[x]=scc;
+            res.push_back(comp[0]);
+        }
+    }
+
+    if(scc==1) {
+        cout << "YES\n"; return;
+    }
+    cout << "NO\n";
+
+    for(int u=1; u<=n; u++) {
+        for(auto v:g[u]) {
+            if(idx[u]!=idx[v]) {
+                cout << v << " " << u << "\n";
+                return;
+            }
+        }
+    }
+    cout << res[0] << " " << res[1] << "\n";
+}
+
+int main() {
+    FastIO
+
+
+    //int TT=1; cin>>TT; while(TT--)
+    solve();
+
+    return 0;
+}
