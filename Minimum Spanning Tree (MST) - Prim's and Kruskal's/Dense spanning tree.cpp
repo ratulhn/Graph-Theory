f691afb8698111b156d394ceb38b498ebe10d244
ratulhn <ratulhossain.bd2003@gmail.com>
2026-05-29 12:13:32 +0600

    Create Dense spanning tree.cpp

---
+++ Minimum Spanning Tree (MST) - Prim's and Kruskal's/Dense spanning tree.cpp
@@ -0,0 +1,59 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+const int N = 3e5 + 9, mod = 1e9;
+
+struct dsu {
+  vector<int> par, rnk, size; int c;
+  dsu(int n) : par(n+1), rnk(n+1,0), size(n+1,1), c(n) {
+    for (int i = 1; i <= n; ++i) par[i] = i;
+  }
+  int find(int i) { return (par[i] == i ? i : (par[i] = find(par[i]))); }
+  bool same(int i, int j) { return find(i) == find(j); }
+  int get_size(int i) { return size[find(i)]; }
+  int count() { return c; } //connected components
+  int merge(int i, int j) {
+    if ((i = find(i)) == (j = find(j))) return -1; else --c;
+    if (rnk[i] > rnk[j]) swap(i, j);
+    par[i] = j; size[j] += size[i];
+    if (rnk[i] == rnk[j]) rnk[j]++;
+    return j;
+  }
+};
+
+int32_t main() {
+  ios_base::sync_with_stdio(0);
+  cin.tie(0);
+  int n, m; cin >> n >> m;
+  vector<array<int, 3>> ed;
+  for(int i = 1; i <= m; i++){
+    int u, v, w; cin >> u >> v >> w;
+    ed.push_back({w, u , v});
+  }
+  sort(ed.begin(), ed.end());
+  long long ans = 1e18;
+
+  for(int i=0; i<m; i++) {
+    int cnt=n-1;
+    dsu d(n);
+    for(int j=i; j<m; j++) {
+        if(!d.same(ed[j][1],ed[j][2])) {
+            cnt--;
+            d.merge(ed[j][1],ed[j][2]);
+            if(cnt==0) {
+                ans=min(ans,(long long)ed[j][0]-ed[i][0]);
+                break;
+            }
+        }
+    }
+     if(cnt) break;
+  }
+
+  if(ans==1e18) cout << "NO\n";
+    else {
+        cout << "YES\n" << ans << "\n";
+    }
+
+
+  return 0;
+}
